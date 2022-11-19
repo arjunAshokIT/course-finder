52fb0201d8d8260bf22c9e7612e2fe2ef8a7e5c2
arjun.ashok <arjun.ashok.it@gmail.com>
2022-11-19 07:28:15 +0530

    Add dummy json data for courses

--- script.js
+++ script.js
@@ -9,11 +9,33 @@ function searchCourse(e) {
   e.preventDefault();
   const searchTerm = search.value;
 
+  if (searchTerm.trim()) {
+    fetch(`courses.json`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonData) => {
+        console.log(jsonData);
 
-  if(searchTerm.trim()) {
-    // do work
+        if (jsonData == null || jsonData.length === 0) {
+          resultHeading.innerHTML = `<p class="text-center m-2">There are no search results. Try again!</h2>`;
+        } else if (jsonData && coursesList) {
+          resultHeading.innerHTML = `<p class="text-center m-2">Below are the search results. Try again!</h2>`;
+          coursesList.innerHTML = jsonData.map((course) => {
+            return `
+             <div class='course col-3'>
+             <img src=${course.course_img} />
+             <h5>${course.course_name}</h5>
+             <h6>${course.course_id}</h6>
+
+
+             </div>
+            `;
+          });
+        }
+      });
   } else {
-    alert("Please enter a search value")
+    alert("Please enter a search value");
   }
 }
 
