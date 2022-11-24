149b51e5ca27f1dddd4bca8ad894fb9e8667117f
arjun.ashok <arjun.ashok.it@gmail.com>
2022-11-24 19:10:05 +0530

    JS logic to demo single course details

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const resultHeading = document.getElementById("result-heading");
 const singleCourse = document.getElementById("single-course");
 
 function searchCourse(e) {
+  // e -> submit event
+  // send the form to the backend
+  // we dont want to submit to the backend | default behavior
   e.preventDefault();
   const searchTerm = search.value;
 
@@ -14,26 +17,42 @@ function searchCourse(e) {
       .then((response) => {
         return response.json();
       })
-      .then((jsonData) => {
-        console.log(jsonData);
+      .then((data) => {
+        resultHeading.innerHTML = `<h2>Search results for '${searchTerm}':</h2>`;
 
-        if (jsonData == null || jsonData.length === 0) {
-          resultHeading.innerHTML = `<p class="text-center m-2">There are no search results. Try again!</h2>`;
-        } else if (jsonData && coursesList) {
-          resultHeading.innerHTML = `<p class="text-center m-2">Below are the search results. Try again!</h2>`;
-          coursesList.innerHTML = jsonData.map((course) => {
-            return `
-             <div class='course col-3'>
-             <img src=${course.course_img} />
-             <h5>${course.course_name}</h5>
-             <h6>${course.course_id}</h6>
+        const myCourse = data.find((course) => {
+          return course.course_name == searchTerm;
+        });
 
-
-             </div>
-            `;
-          });
+        if (myCourse == undefined) {
+          resultHeading.innerHTML = `<p>There are no search results. Please try again</p>`;
+          coursesList.innerHTML = "";
+        } else {
+          coursesList.innerHTML = `
+          <hr/>
+          <h2>${myCourse.course_name}</h2>
+          <p>${myCourse.course_id}</p>
+          <img src="${myCourse.course_img}"  height=200 width=200/> 
+          `;
         }
       });
+
+    // if (jsonData == null || jsonData.length === 0) {
+    //   resultHeading.innerHTML = `<p class="text-center m-2">There are no search results. Try again!</h2>`;
+    // } else if (jsonData && coursesList) {
+    //   resultHeading.innerHTML = `<p class="text-center m-2">Below are the search results. Try again!</h2>`;
+    //   coursesList.innerHTML = jsonData.map((course) => {
+    //     return `
+    //      <div class='course col-3'>
+    //      <img src=${course.course_img} />
+    //      <h5>${course.course_name}</h5>
+    //      <h6>${course.course_id}</h6>
+
+    //      </div>
+    //     `;
+    //   });
+    // }
+    // });
   } else {
     alert("Please enter a search value");
   }
